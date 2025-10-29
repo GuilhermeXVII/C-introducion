9458d31099f74e23847e0db14dec09a61ea73a80
𝐺𝑢𝑖𝑙ℎ𝑒𝑟𝑚𝑒 <150200952+GuilhermeXVII@users.noreply.github.com>
2025-10-29 11:59:44 -0300

    Add files via upload

---
+++ jogodaadvinhacao.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "************************************"<<endl;
+    cout << "* Bem-vindos ao jogo da adivinhacao! *"<<endl;
+    cout << "************************************"<<endl;
+    cout<<"Meu primeiro programa em c++"<<endl;
+
+    int numero_secreto=42;
+    cout << "O numero secreto e"<<numero_secreto<<", nao conte para ninguem"<<endl;
+
+    double multiplicao = 7 * 2.43;
+    cout<<multiplicao<<endl;
+
+    char letra = 66;
+    cout<<letra<<endl;
+
+    int numeroSecreto = 65*8;
+    cout<<"O número multiplicado é:"<<numero_secreto<<endl;
+
+}
+
+
